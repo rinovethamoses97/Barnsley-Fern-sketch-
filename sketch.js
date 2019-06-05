cfe06f14f50d54827d0dfaffb7e71404678a9456
rino <rinovethamoses97@gmail.com>
2019-06-05 21:16:06 +0530

    initial commit

---
+++ sketch.js
@@ -0,0 +1,36 @@
+var x=0;
+var y=0;
+function setup(){
+    createCanvas(600,600);
+    background(0);
+}
+function nextPoint(x,y){
+    var nextx;
+    var mexty;
+    var r=random(1);
+    if(r<0.01){
+        nextx=0;
+        nexty=0.16*y;
+    }
+    else if(r<0.86){
+        nextx=0.85*x+0.04*y;
+        nexty=-0.04*x+0.85*y+1.6;
+    }
+    else if(r<0.93){
+        nextx=0.2*x-0.26*y;
+        nexty=0.23*x+0.22*y+1.6;
+    }
+    else{
+        nextx=-0.15*x+0.28*y;
+        nexty=0.26*x+0.24*y+0.44;
+    }
+    return [nextx,nexty];
+}
+function draw(){
+    x=map(x,-2.1820,2.6558,0,width);
+    y=map(y,0,9,9983,height,0);
+    point(x,y);
+    var next=nextPoint(x,y);
+    x=next[0];
+    y=next[i];
+}
